4de2a4c15ae1819052999b3e6c2967e4106b728d
emileebogdan <emileebogdan@gmail.com>
2022-11-25 11:17:34 -0700

    added attribution comment

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,14 +1,12 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     console.log("before install");
     window.deferredPrompt = event;
     butInstall.removeAttribute('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
     if(!promptEvent) {
@@ -19,7 +17,9 @@ butInstall.addEventListener('click', async () => {
     butInstall.removeAttribute('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     window.deferredPrompt = null;
 });
+
+
+// thanks to Jerrod Linderman and Logan Schoerner for help
